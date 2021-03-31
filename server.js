3fe988670f5c7b9f6efb326b8edbcdc0bd90b7e3
Clint <maxeva02@gmail.com>
2021-03-30 20:09:21 -0700

    updating files

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const routes = require("./controllers/workouts_controller.js");
 app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
-app.listen(PORT, function () {
+app.listen(PORT, function() {
     // Log (server-side) when our server has started
     console.log("Server listening on port " + PORT);
 });
