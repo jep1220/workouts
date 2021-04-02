c01c137b73bf3998b58cdd341292a4d4b8368bc6
jep1220 <38770396+jep1220@users.noreply.github.com>
2021-04-01 22:08:20 -0700

    pulled code and pushed updated db folder

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,2 +1,11 @@
 INSERT INTO workouts (workout_name, crushed)
 VALUES("bench press", 1), ("cardio", 1), ("weights, chest and back", 1)
+
+
+
+INSERT INTO signup (user_name, email, pass_word)
+VALUES("newuser", "new@user.com", "lemmejoin");
+
+INSERT INTO users (user_name, email, pass_word)
+VALUES("ernieperez", "ernie@perez.com", "perez"), ("crizzo", "clint@rizzo.com", "clint"), ("smahmody", "shakofa@mahmody.com", "shakofa") , ("danelaban", "dane@laban.com", "laban");
+
